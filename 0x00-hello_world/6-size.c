db6b8090d663ce3419b681e81ee65eebfea16bc5
Samuel <samuel.srmv@gmail.com>
2020-02-05 21:00:34 +0000

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - coment
+ * Return: value 0
+ */
+int main(void)
+prinft("size of char datatype is: %1d byte\n, sizeof size");
+prinft("size of char datatype is: %3d byte\n, sizeof size");
+prinft("size of char datatype is: %4d byte\n, sizeof size");
+prinft("size of char datatype is: %5d byte\n, sizeof size");
+prinft("size of char datatype is: %6d byte\n, sizeof size");
+return (0);
