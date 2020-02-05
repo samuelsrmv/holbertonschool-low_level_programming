98182a1602e6aca134fbcddc62a1119f9e8d8934
Samuel <samuel.srmv@gmail.com>
2020-02-05 22:30:19 +0000

    UNIX is basically a simple operating system, but you have to be

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include <stdio.h>
 /**
- * main - function 
+ * main - function
  * return: value 0
  */
-int main (void)
+int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
