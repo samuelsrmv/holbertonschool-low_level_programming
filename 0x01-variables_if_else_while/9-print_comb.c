2a53e2de37bbab768b9a2570a439bb41199031f9
Samuel <samuel.srmv@gmail.com>
2020-02-07 03:32:14 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - coments
+ * Return: value 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar((char) (num + 48));
+if (num < 9)
+{
+putchar(44);
+putchar(32);
+}
+if (num == 9)
+{
+putchar('\n');
+}
+}
+return (0);
+}
