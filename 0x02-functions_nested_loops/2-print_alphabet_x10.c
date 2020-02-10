e8aa17d68cf3894ca6b69dabb134522dee0e2bfa
Samuel <samuel.srmv@gmail.com>
2020-02-10 22:32:57 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - coment
+ * Return: value 0
+ */
+void print_alphabet(void)
+{
+char c;
+int i;
+for (i = 0; i <= 9; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
