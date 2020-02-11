663ea4095aef83f5176a156038f38aaaf066585e
Samuel <samuel.srmv@gmail.com>
2020-02-11 20:12:50 +0000

    There is no such thing as absolute value in this world

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _abs - print abs
+ * @n: fuction
+ * Return: value 0
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * (-1);
+return (n);
+}
+else if (n > 0)
+{
+return (n);
+}
+else
+{
+return (0);
+}
+return (0);
+}
