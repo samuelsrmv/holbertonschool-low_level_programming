a1d855a93072c920c966a92cc7e9f929fe38190e
Samuel <samuel.srmv@gmail.com>
2020-02-11 18:58:09 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * print_sign - coment
- * @n: fuction 
+ * print_sign - print sing
+ * @n: fuction
  *Return: value 0
  */
 int print_sign(int n)
@@ -11,7 +11,7 @@ if (n > 0)
 _putchar(43);
 return (1);
 }
- else if (n < 0)
+else if (n < 0)
 {
 _putchar(45);
 return (-1);
@@ -19,7 +19,7 @@ return (-1);
 else
 {
 _putchar(48);
-return(0);
+return (0);
 }
 return (0);
 }
