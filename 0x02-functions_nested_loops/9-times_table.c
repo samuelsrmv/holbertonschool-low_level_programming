7145fe529b7d558a0a1870cf0a52e96e1a151be7
Samuel <samuel.srmv@gmail.com>
2020-02-12 00:05:42 +0000

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+ * print_times_table - function prints
+ * Return: void
+ * @n: return something
+ */
+void print_times_table(int n)
+{
+int i, j, prod;
+prod = 1;
+if (!(n > 15))
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+prod = i * j;
+if (prod > 9)
+{
+_putchar(44);
+_putchar(32);
+if (prod < 100)
+_putchar(32);
+if (prod > 99)
+{
+_putchar((char) (prod / 100) + 48);
+}
+_putchar((char) ((prod / 10) % 10) + 48);
+_putchar((char) (prod % 10) + 48);
+}
+else
+{
+if (j > 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+_putchar(32);
+}
+_putchar((char) (prod + 48));
+}
+}
+_putchar('\n');
+}
+}
+}
