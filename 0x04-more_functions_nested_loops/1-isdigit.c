6a9fddb835bb4ccaee1870255a8dda01039138c6
Samuel <samuel.srmv@gmail.com>
2020-02-13 17:39:01 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - printf isdigit
+ * @c: fuction
+ * Return: Value 0
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
