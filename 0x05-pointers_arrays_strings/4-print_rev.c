9cf476794d1ed94c5cead19abc85d6a57deac0e1
Samuel <samuel.srmv@gmail.com>
2020-02-18 20:55:44 +0000

    I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,23 @@
 #include "holberton.h"
 /**
  * print_rev - pointers
- *@s: pointers
  * Return: value 0
+ * @s: pointers
  */
 void print_rev(char *s)
 {
+int i;
 int b;
-for (b = 62; s[b] != '\0'; b--)
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+i--;
+for (b = i; b != -1; b--)
 {
 _putchar(s[b]);
 }
 _putchar('\n');
 }
+
