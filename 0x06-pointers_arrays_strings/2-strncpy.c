e34254d32a1498c06147078663ac6c0e9a49d957
Samuel <samuel.srmv@gmail.com>
2020-02-20 21:45:17 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strncat - pointers
+ * _strncpy - pointers
  * @dest: pointers
  * @src: pointers
  * @n: pointers
@@ -10,14 +10,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 int c;
-
 for (i = 0; dest[i] != '\0'; i++)
-; 
+;
 for (c = 0; c < n && src[c] != '\0'; c++)
 {
-dest[c] = src[c]; 
+dest[c] = src[c];
 }
- for (; c < n; c++)
+for (; c < n; c++)
 dest[c] = '\0';
 return (dest);
 }
