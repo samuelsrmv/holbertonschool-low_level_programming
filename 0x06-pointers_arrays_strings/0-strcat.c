0d8b7d4936d83283e0f464222df645bc8177a22f
Samuel <samuel.srmv@gmail.com>
2020-02-20 17:42:51 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 /**
- * *_stract - pointers
- * : pointers
- * Return: void
+ * _strcat - pointers
+ * @dest: pointers
+ * @src: pointers
+ * Return: value
  */
 char *_strcat(char *dest, char *src)
 {
-  int i;
-  int c;
-  int j;
-for (i = 0; dest[i] != '\0'; i++)  
-  ;
+int i;
+int c;
+int j;
+for (i = 0; dest[i] != '\0'; i++)
+;
 for (c = 0; src[c] != '\0'; c++)
-  ;
+;
 for (j = i; j < (i + c); j++)
 {
 dest[j] = src[j - i];
