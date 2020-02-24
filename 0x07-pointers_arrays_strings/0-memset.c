08f8653837648af5089d8c159d200c3fe984a3ed
Samuel <samuel.srmv@gmail.com>
2020-02-24 17:39:49 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *_memset - pointer
+ * @n: pointers
+ * @s: pointer
+ * @n: pointers
+ * @b: pointers
+ * Return: value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int c;
+for (c = 0; c < n; c++)
+;
+s[c] = b;
+{
+return (s);
+}
+}
