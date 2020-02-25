e053bca9f962bf5d64947c6eb2eb63c025936edc
Samuel <samuel.srmv@gmail.com>
2020-02-25 04:49:27 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *_strspn - pointer
+ * @s: pointers
+ * @accept: pointers
+ * Return: value
+ */
+char *_strstr(char *haystack, char *needle)
+{
+  int i = 0, j = 0, a = 0;
+while (haystack[a] != '\0')
+{
+i = a;
+j = 0;
+while (haystack[i] != '\0' && needle[j] != '\0' && haystack[i] == needle[j])
+{
+i++;
+j++;
+}
+if (needle[j] == '\0')
+return (haystack + a);
+a++;
+}
+return ('\0');
+}
