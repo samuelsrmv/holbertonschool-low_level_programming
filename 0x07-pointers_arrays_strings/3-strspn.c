906d585c5384d1661347958191dc49b033ab23a3
Samuel <samuel.srmv@gmail.com>
2020-02-25 04:04:22 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * *_strspn - pointer
+ * @s: pointers
+ * @accept: pointers
+ * Return: value
+ */
+{
+unsigned int _strspn(char *s, char *accept)
+{
+int k = 0, m = 0;
+int c = 0, b = 0;
+while (s[k] != '\0')
+{
+m = 0;
+b = 1;
+while (accept[m] != '\0')
+{
+if (s[k] == accept[m])
+{
+c++;
+b = 0;
+}
+m++;
+}
+if (b == 1)
+break;
+k++;
+}
+return (c);
+}
