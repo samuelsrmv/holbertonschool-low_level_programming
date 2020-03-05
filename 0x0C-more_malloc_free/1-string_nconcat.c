29b0908ecf714054d019fab9dda1633a1823c701
samuelsrmv <samuel.srmv@gmail.com>
2020-03-05 21:14:11 +0000

    function that concatenates

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,11 +14,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		return ("");
+		s2 = "";
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
