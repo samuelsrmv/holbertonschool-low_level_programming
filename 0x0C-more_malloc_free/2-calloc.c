eec681a92983b619aa6dd575f4bc3ea1a30154d3
samuelsrmv <samuel.srmv@gmail.com>
2020-03-05 23:08:32 +0000

    function that allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		free(a);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < nmemb * size; i++)
 		a[i] = 0;
 return (a);
 }
