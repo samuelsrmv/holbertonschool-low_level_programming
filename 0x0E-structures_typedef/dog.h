f78c857ee55e2ba50f413d1514fe3f620adbdb94
samuelsrmv <samuel.srmv@gmail.com>
2020-03-09 21:32:36 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,13 @@
  * @owner: value
  * Return: value
  */
-dog_t
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
