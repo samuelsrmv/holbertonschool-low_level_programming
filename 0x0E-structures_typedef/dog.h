780b5e3b5905c517ef1ef302da74eebe9e3f4526
samuelsrmv <samuel.srmv@gmail.com>
2020-03-09 19:01:02 +0000

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/**
+ * struct dog - struct dog
+ * @name: value
+ * @age: value
+ * @owner: value
+ * Return: value
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
