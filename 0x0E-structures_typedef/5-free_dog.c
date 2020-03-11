62f4f973134a2af78a0ed38ea4a768538460c224
Samuel <samuel.srmv@gmail.com>
2020-03-11 02:51:10 +0000

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free_dog
+ * @d: pointers
+ * Return: Value
+ * Description: Fuction
+ */
+void free_dog(dog_t *d);
+{
+	free(dog_t);
+	free(d);
+	return;
+}
