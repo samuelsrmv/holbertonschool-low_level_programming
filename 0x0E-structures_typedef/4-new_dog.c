09cb9bc0d4992d033795f1045d102246709aed14
Samuel <samuel.srmv@gmail.com>
2020-03-11 01:45:04 +0000

    unction that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - print_dog
+ * @d: pointers
+ * Description: Fuction
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  dog_t *dog;
+  int i, j, c;
+    
+  dog = malloc(sizeof(dog_t));
+  if (dog == NULL)
+    {    
+      return (NULL);
+    }
+    for (i = 0; name[i] != '\0'; i++)
+      ;
+    for (j = 0; owner[j] != '\0'; j++)
+      ;
+      dog->name = malloc(sizeof(char) * i + 1);
+    
+    if (dog->name == NULL)
+    {
+      free(dog);
+      return (NULL);
+    }
+    dog->owner = malloc(sizeof(char) * j + 1);
+  {
+    free(dog->name);
+    free(dog);
+    return (NULL);
+  }
+  
+  for (c = 0; c < i; c++)
+    dog->name[c] = name[c];
+  dog->name[c] = '\0';
+  
+  dog->age = age;
+ 
+  for(c = 0; c < j; c++)
+    dog->owner[c] = owner[c];
+  dog->owner[c] = '\0';
+  
+  return (dog);
+}
