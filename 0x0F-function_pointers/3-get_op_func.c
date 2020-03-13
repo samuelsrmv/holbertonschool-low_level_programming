8f95e42f5df1ab97da230c418bd6096f1cdecbea
samuelsrmv <samuel.srmv@gmail.com>
2020-03-13 22:48:01 +0000

    3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func - function principal
+ * @s: noperators
+ * Return: int
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op)
+		{
+			return ((ops[i].f));
+		}
+		i++;
+	}
+	return (0);
+}
