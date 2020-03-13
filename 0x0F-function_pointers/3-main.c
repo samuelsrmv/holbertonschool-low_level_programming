071f0880c94cff8b7c8fcc2b6c38fe101b100cc9
samuelsrmv <samuel.srmv@gmail.com>
2020-03-13 22:47:04 +0000

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "calc.h"
+#include <stdlib.h>
+/**
+ * main - function principal
+ * @argc: numbers of parameters
+ * @argv: arrays of arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*argv[2] != '+' && *argv[2] != '-'
+	     && *argv[2] != '*' && *argv[2] != '/'
+	     && *argv[2] != '%') || (argv[2][1] != '\0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(num1, num2));
+	return (0);
+}
