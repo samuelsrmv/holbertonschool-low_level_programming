184152c9f1d40754f08cf71323dd7c0e33be96be
samuelsrmv <samuel.srmv@gmail.com>
2020-03-13 22:47:35 +0000

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+/**
+ * op_add - add two numbers
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub two numbers
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mult two numbers
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - add two numbers
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod two numbers
+ * @a: first value
+ * @b: second value
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
