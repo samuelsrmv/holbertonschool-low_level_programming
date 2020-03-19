f2027764faecef126751cd51327a6e355aef14da
Samuel <samuel.srmv@gmail.com>
2020-03-19 23:45:43 +0000

    function that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print_list - print_list
+ * @h: pinters
+ * Return: Value
+ * Description: Fuction
+ */
+void free_list(list_t *head)
+{
+	free(*head);
+	free(list_t);
+}
