e8673d6fc749e8f8ce7ca7ec671788667cacae0a
Samuel <samuel.srmv@gmail.com>
2020-03-19 20:47:44 +0000

    function that returns the number of elements in a linked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - list_len
+ * @h: pointer
+ * Return: Value
+ * Description: Fuction
+ */
+size_t list_len(const list_t *h)
+{
+	int x;
+
+	for (x = 1; h->next != NULL; x++)
+	{
+		h = h->next;
+	}
+return (x);
+}
