79dfd550495204d933ee094f533fdcd19396696d
Samuel <samuel.srmv@gmail.com>
2020-03-24 00:29:35 +0000

    function that adds a new node at the end of a listint_t list.

---
+++ 0x13-more_singly_linked_list/3-add_nodeint_end.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * print_listint - print_listint
+ * @h: pointers
+ * Return: Value
+ * Description: Fuction
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
