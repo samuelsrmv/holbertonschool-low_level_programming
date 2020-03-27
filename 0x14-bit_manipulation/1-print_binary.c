1a24b01c12c70879b87069aca03703ed1f44a18a
Samuel <samuel.srmv@gmail.com>
2020-03-27 04:56:11 +0000

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_binary - print_binary
+ * @n: pointers
+ * Return: Value
+ * Description: Fuction
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0 || n == 1)
+	{
+		_putchar(n + '0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+
+}
