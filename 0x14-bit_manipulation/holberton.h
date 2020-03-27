793c8cd2bd14be108d859edcbdd2e528a13b7426
Samuel <samuel.srmv@gmail.com>
2020-03-27 04:48:21 +0000

    holberton.h

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+#endif
