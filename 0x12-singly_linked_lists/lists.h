9ef272be844c117b16f443037b8793c25f485145
Samuel <samuel.srmv@gmail.com>
2020-04-15 16:31:39 +0000

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,4 +27,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int large(const char *str);
 void free_list(list_t *head);
+list_t *add_node_end(list_t **head, const char *str);
+int _strlen(const char *str);
 #endif
