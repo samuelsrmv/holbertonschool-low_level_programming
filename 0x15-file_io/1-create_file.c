9f925f674c378a74a8ff18d8135edeccd55459f7
Samuel <samuel.srmv@gmail.com>
2020-04-15 15:57:55 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  *create_file - function
  *@filename: pointer
- *@text_content pointer
+ *@text_content: pointer
  * Return: int.
  */
 int create_file(const char *filename, char *text_content)
