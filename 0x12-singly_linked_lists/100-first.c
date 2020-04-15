6b68e8b9d3329a0e207edaccd9e1928c960c59d4
Samuel <samuel.srmv@gmail.com>
2020-04-15 16:37:29 +0000

    function that prints

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void myStartupFun(void) __attribute__ ((constructor));
+/**
+ * myStartupFun -myStartupfun
+ * @void: void
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
