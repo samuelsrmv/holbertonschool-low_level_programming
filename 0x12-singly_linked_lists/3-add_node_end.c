cd942d6d19ac376d9fdd7c248c3f701138759029
Samuel <samuel.srmv@gmail.com>
2020-04-15 16:31:11 +0000

    function that adds a new node

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+/**
+ * add_node_end - function
+ * @head: pointr 
+ * @str: pointer
+ * Return: list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+
+	tmp = *head;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	(*new).str = strdup(str);
+	(*new).len = _strlen(str);
+	(*new).next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+	return (*head);
+}
+/**
+ * _strlen - function
+ * @str: pointer
+ * Return: int
+ */
+int _strlen(const char *str)
+{
+	int i;
+
+	while (*str)
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
