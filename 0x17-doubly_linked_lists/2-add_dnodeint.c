84da96fd31de4b36170674b8c8c7bee4bc3a33f3
samuelsrmv <samuel.srmv@gmail.com>
2020-12-13 14:49:00 -0500

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,16 +8,16 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *node_samu;
-node_samu = malloc(sizeof(dlistint_t));
-if (node_samu == NULL)
-{
-printf("Error: Can't malloc\n");
-return (NULL);
-}
-node_samu->n = n;
-node_samu->prev = NULL;
-node_samu->next = *head;
-*head = node_samu;
+    dlistint_t *node_samu;
+    node_samu = malloc(sizeof(dlistint_t));
+    if (node_samu == NULL)
+        {
+            printf("Error: Can't malloc\n");
+            return (NULL);
+        }
+    node_samu->n = n;
+    node_samu->prev = NULL;
+    node_samu->next = *head;
+    *head = node_samu;
 return (node_samu);
 }
