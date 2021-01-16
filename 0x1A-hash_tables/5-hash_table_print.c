a2d9fcf7d98a277135b6009ec4c6a1dd97f06b85
samuelsrmv <samuel.srmv@gmail.com>
2021-01-15 22:52:04 -0500

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print hash table
+ * @ht: hash table
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node_table;
+	unsigned int idx = 0;
+	unsigned int aux = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	node_table = ht->array[0];
+	putchar('{');
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		node_table = ht->array[idx];
+		while (node_table)
+		{
+			if (aux == 1)
+				printf(", ");
+			printf("'%s': '%s'", node_table->key, node_table->value);
+			aux = 1;
+			node_table = node_table->next;
+		}
+	}
+	putchar('}');
+	putchar('\n');
+}
